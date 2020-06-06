6b459e676bfea4ab68734b621890d711afafd5b0
vincentwinberg <winbergvincent@gmail.com>
2020-06-06 21:18:23 +0200

    saker funkar

--- interface.py
+++ interface.py
@@ -1,7 +1,5 @@
 from tkinter import *
 from tkinter import ttk 
-import functions as func
-
 
 class MainGUI():
     def __init__(self, master):
@@ -9,38 +7,43 @@ class MainGUI():
         self.master = master
         master.title("Calculator")
 
+        self.saved_val = 0
+        self.curr_op = ""
+        self.curr_val = 0
+        self.result = 0
+    
         # Main interface starts here
         ''' Row 1 '''
-        self.lb1 = Label(master, text="0")
+        self.lb1 = Label(master, text="")
 
         ''' Row 2 '''
-        self.reset_btn = Button(master, text="AC", width=5, height=2)
+        self.reset_btn = Button(master, text="AC", width=5, height=2, command=lambda: self.clear_window())
         self.pm_btn = Button(master, text="+/-", width=5, height=2)
         self.percent_btn = Button(master, text="%", width=5, height=2)
-        self.div_btn = Button(master, text="/", width=5, height=2)
+        self.div_btn = Button(master, text="/", width=5, height=2, command=lambda: self.division())
 
         ''' Row 3 '''
-        self.seven = Button(master, text="7", width=5, height=2)
-        self.eight = Button(master, text="8", width=5, height=2)
-        self.nine = Button(master, text="9", width=5, height=2)
-        self.mult_btn = Button(master, text="*", width=5, height=2)
+        self.seven = Button(master, text="7", width=5, height=2, command=lambda: self.btn_press(7))
+        self.eight = Button(master, text="8", width=5, height=2, command=lambda: self.btn_press(8))
+        self.nine = Button(master, text="9", width=5, height=2, command=lambda: self.btn_press(9))
+        self.mult_btn = Button(master, text="*", width=5, height=2, command=lambda: self.multiplication())
 
         ''' Row 4 '''
-        self.four = Button(master, text="4", width=5, height=2)
-        self.five = Button(master, text="5", width=5, height=2)
-        self.six = Button(master, text="6", width=5, height=2)
-        self.sub_btn = Button(master, text="-", width= 5, height=2)
+        self.four = Button(master, text="4", width=5, height=2, command=lambda: self.btn_press(4))
+        self.five = Button(master, text="5", width=5, height=2, command=lambda: self.btn_press(5))
+        self.six = Button(master, text="6", width=5, height=2, command=lambda: self.btn_press(6))
+        self.sub_btn = Button(master, text="-", width= 5, height=2, command=lambda: self.subtraction())
 
         ''' Row 5 '''
         self.one = Button(master, text="1", width=5, height=2, command=lambda: self.btn_press(1))
-        self.two = Button(master, text="2", width=5, height=2)
-        self.three = Button(master, text="3", width=5, height=2)
-        self.plus_btn = Button(master, text="+", width=5, height=2)
+        self.two = Button(master, text="2", width=5, height=2, command=lambda: self.btn_press(2))
+        self.three = Button(master, text="3", width=5, height=2, command=lambda: self.btn_press(3))
+        self.add_btn = Button(master, text="+", width=5, height=2, command=lambda: self.addition())
 
         ''' Row 6 '''
         self.zero = Button(master, text="0", width=11, height=2)
         self.dec_btn = Button(master, text=",", width=5, height=2)
-        self.eq_btn = Button(master, text="=", width=5, height=2)
+        self.eq_btn = Button(master, text="=", width=5, height=2, command=lambda: self.eq_func())
 
         # Aligns with grid
         self.lb1.grid(row=0, column=0, columnspan=4)
@@ -53,20 +56,79 @@ class MainGUI():
         self.nine.grid(row=2, column=2)
         self.mult_btn.grid(row=2, column=3)
         self.four.grid(row=3, column=0)
-        self.four.grid(row=3, column=1)
+        self.five.grid(row=3, column=1)
         self.six.grid(row=3, column=2)
         self.sub_btn.grid(row=3, column=3)
         self.one.grid(row=4, column=0)
         self.two.grid(row=4, column=1)
         self.three.grid(row=4, column=2)
-        self.plus_btn.grid(row=4, column=3)
+        self.add_btn.grid(row=4, column=3)
         self.zero.grid(row=5, column=0, columnspan=2)
         self.dec_btn.grid(row=5, column=2)
         self.eq_btn.grid(row=5, column=3)
 
     # Methods
+    def clear_window(self):
+        self.lb1["text"] = ""
+        self.saved_val = 0
+
     def btn_press(self, num):
-        self.lb1["text"] += "{}".format(num)
+        if len(self.lb1["text"]) >= 9:
+            return False
+        else:
+            self.lb1["text"] += "{}".format(num)
+
+    def addition(self):
+        if len(self.lb1["text"]) == 0:
+            return False
+        else:
+            self.saved_val = float(self.lb1["text"])
+            self.curr_op = "add"
+            self.lb1["text"] = ""
+
+    def subtraction(self):
+        if len(self.lb1["text"]) == 0:
+            return False
+        else:
+            self.saved_val = float(self.lb1["text"])
+            self.curr_op = "sub"
+            self.lb1["text"] = ""
+
+    def multiplication(self):
+        if len(self.lb1["text"]) == 0:
+            return False
+        else:
+            self.saved_val = float(self.lb1["text"])
+            self.curr_op = "mult"
+            self.lb1["text"] = ""
+
+    def division(self):
+        if len(self.lb1["text"]) == 0:
+            return False
+        elif float(self.lb1["text"]) == 0:
+            print("Du kan inte dividera med noll!")
+            return False
+        else:
+            self.saved_val = float(self.lb1["text"])
+            self.curr_op = "div"
+            self.lb1["text"] = ""
+
+    def eq_func(self):
+        self.curr_val = float(self.lb1["text"])
+
+        if self.curr_op == "add":
+            self.result = self.saved_val + self.curr_val
+
+        if self.curr_op == "sub":
+            self.result = self.saved_val - self.curr_val
+
+        if self.curr_op == "mult":
+            self.result = self.saved_val * self.curr_val
+
+        if self.curr_op == "div":
+            self.result = self.saved_val / self.curr_val
+
+        self.lb1["text"] = str(self.result)
 
 
 # Runs tkinter
